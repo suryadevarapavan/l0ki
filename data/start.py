4e74b838952739e026a24994d653e70fb6a16527
Suryadevara Pavan <suryadevarapavan770@gmail.com>
2025-07-19 06:10:40 +0530

    Update start.py
    It can stores server logs with their specific types respectively.

--- data/start.py
+++ data/start.py
@@ -1,8 +1,15 @@
-path="/var/log/syslog"
+#path="/var/log/syslog"
+#path="/var/log/auth.log"
+#path="/var/log/dmesg"
+#path="/var/log/kern.log"
+path="/var/log/Xorg.0.log"
+
+fn=os.path.basename(path)
+
 with open(path,"r") as f:
     data=f.read()
     print(f.read())
-    with open("syslog.txt","w") as f1:
+    with open(f"{fn}.txt","w") as f1:
         f1.write(data)
     f1.close()
 f.close()
