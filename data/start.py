9e2e2aa784997c3bfc8e28644076b230ae80f00d
Suryadevara Pavan <suryadevarapavan770@gmail.com>
2025-07-22 18:34:37 +0530

    Update start.py
    I added features and try to reduce time complexity

--- data/start.py
+++ data/start.py
@@ -1,15 +1,19 @@
-#path="/var/log/syslog"
-#path="/var/log/auth.log"
-#path="/var/log/dmesg"
-#path="/var/log/kern.log"
-path="/var/log/Xorg.0.log"
+import os
 
-fn=os.path.basename(path)
+logs=["syslog","auth.log","dmesg","kern.log","Xorg.0.log"]
 
-with open(path,"r") as f:
-    data=f.read()
-    print(f.read())
-    with open(f"{fn}.txt","w") as f1:
+
+
+def store_data(path):
+    fn=os.path.basename(path)
+    with open(path,"r") as f:
+        data=f.read()
+    with open(f"{fn}.txt","w")as f1:
         f1.write(data)
-    f1.close()
-f.close()
+        f1.close()
+        f.close()
+
+for i in logs:
+    path=f"/var/log/{i}"
+    store_data(path)
+print("program executed")
